e7c898000c96bf37c8f847c82be265dc0a3f9650
Tim Perkins <timperkins10@outlook.com>
2022-12-26 11:24:31 +1100

    Migrate to tsup

---
+++ .changeset/tame-mugs-teach.md
@@ -0,0 +1,5 @@
+---
+"rehype-section-headings": minor
+---
+
+Migrate to tsup for bundling
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig } from "vitest/config";
-import pkg from "./package.json";
 
 export default defineConfig({
-	build: {
-		emptyOutDir: true,
-		outDir: "dist",
-		lib: {
-			name: pkg.name,
-			entry: "./src/index.ts",
-			fileName: "index",
-		},
-	},
 	test: {
 		coverage: {
 			statements: 100,
